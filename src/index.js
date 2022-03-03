8a3dadffdcfa7e566f8c857c24bcfdb1c1b7b2d7
Aaron Boyd <a.boyd91@yahoo.com>
2022-03-03 06:04:05 -0800

    add template

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './../css/styles.css';
+// import CLASS from './business.js';
+import $ from 'jquery';
+
+//UI Logic
+
+
+
